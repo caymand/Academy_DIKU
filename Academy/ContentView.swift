6fe7f5b44fad5193b98d59b64fea7c5241e5c2c8
Kristoffer August Kortbaek <kak@hotmail.dk>
2020-03-31 13:03:12 +0200

    Initial Commit

---
+++ Academy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Academy
+//
+//  Created by Kristoffer Kortbaek on 31/03/2020.
+//  Copyright © 2020 Kristoffer Kortbaek. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
